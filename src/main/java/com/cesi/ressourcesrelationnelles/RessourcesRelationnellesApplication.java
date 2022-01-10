2a5c23751fd92fa63e2f47f2c068d3663182081c
paul <xls-pauul-xls@hotmail.fr>
2022-01-10 12:07:33 +0100

    Add Spring Boot

---
+++ src/main/java/com/cesi/ressourcesrelationnelles/RessourcesRelationnellesApplication.java
@@ -0,0 +1,13 @@
+package com.cesi.ressourcesrelationnelles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RessourcesRelationnellesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RessourcesRelationnellesApplication.class, args);
+	}
+
+}
