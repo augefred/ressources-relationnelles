0b9a85308788af6e4b8d981f2892bda82301d427
augefred <frederic.auge@viacesi.fr>
2022-01-10 12:07:40 +0100

    ajout Spring boot

---
+++ src/test/java/com/cesi/ressourcesrelationnelles/RessourcesRelationnellesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesi.ressourcesrelationnelles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RessourcesRelationnellesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
