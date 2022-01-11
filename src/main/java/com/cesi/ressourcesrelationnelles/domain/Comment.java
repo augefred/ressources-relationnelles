3fc5d4e3818ceb9bdff9166e9d7ae6ac63d95f8f
paul <xls-pauul-xls@hotmail.fr>
2022-01-11 16:08:03 +0100

    Add Comment class

--- src/main/java/com/cesi/ressourcesrelationnelles/domain/Comment.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/domain/Comment.java
@@ -5,4 +5,36 @@ public class Comment {
     public String nameUser;
     public String nameUserParentComment;
     public String commentMessage;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getNameUserParentComment() {
+        return nameUserParentComment;
+    }
+
+    public void setNameUserParentComment(String nameUserParentComment) {
+        this.nameUserParentComment = nameUserParentComment;
+    }
+
+    public String getCommentMessage() {
+        return commentMessage;
+    }
+
+    public void setCommentMessage(String commentMessage) {
+        this.commentMessage = commentMessage;
+    }
 }
---
+++ src/test/java/com/cesi/ressourcesrelationnelles/domain/CommentTest.java
@@ -0,0 +1,20 @@
+package com.cesi.ressourcesrelationnelles.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class CommentTest {
+
+    @Test
+    public void createComment() {
+        Comment comment = new Comment();
+        comment.setCommentMessage("Un message généreux");
+        Assert.notNull(comment);
+        assertEquals("Un message généreux", comment.getCommentMessage());
+    }
+
+
+}
