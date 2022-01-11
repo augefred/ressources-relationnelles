3dde77bda5e261ead7930b60177c47eb2f5439cc
Corentin <corentin.castel0@gmail.com>
2022-01-11 12:45:44 +0100

    ajout des focntion d'ajout d'une ressource, de recuperation via un identifiant d'une ressource, recuperation de la liste des ressources

--- src/main/java/com/cesi/ressourcesrelationnelles/controller/RessourcesController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/controller/RessourcesController.java
@@ -1,13 +1,37 @@
 package com.cesi.ressourcesrelationnelles.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.*;
 
 @RestController
 public class RessourcesController {
 
+    HashMap<Long, String> ressources = new HashMap<>();
+
     @GetMapping("/ressources")
-    public String index() {
-        return "Index des ressources";
+    public HashMap<Long, String> getAllRessources() {
+        return ressources;
+    }
+
+    @GetMapping("/ressources/{id}")
+    public String getRessourceById(Long id) {
+        if(ressources.containsKey(id)){
+            return ressources.get(id);
+        }
+        return "Aucune ressource n'a été trouvée";
+    }
+
+    @PostMapping("/ressources")
+    public String addRessource(String ressource) {
+        try{
+            ressources.putIfAbsent((long) ressources.size(), ressource);
+            return "La ressource a été ajoutée";
+        }
+        catch (Exception ex){
+            return "Le commentaire n'a pu être ajouté";
+        }
     }
 }
