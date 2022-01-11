44287f3e0d3d354289ccb7211064376667edb550
paul <xls-pauul-xls@hotmail.fr>
2022-01-11 12:03:55 +0100

    Add controller comment and Test

---
+++ src/main/java/com/cesi/ressourcesrelationnelles/controller/CommentsController.java
@@ -0,0 +1,40 @@
+package com.cesi.ressourcesrelationnelles.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CommentsController {
+
+    @GetMapping("/GetComment?id")
+    public String getCommentById(int id) {
+        String comment;
+        if (id == 10)
+            comment = "Commentaire d'Alfred";
+        else
+            comment = "Aucun commentaire à l'id spécifié";
+
+        return comment;
+    }
+
+    @PostMapping("/GetAllComment")
+    public List<String> getAllComment() {
+        List<String> comments = new ArrayList<>();
+        comments.add("Commentaire de Jack");
+        comments.add("Commentaire de Antoine");
+        comments.add("Commentaire de Sylvie");
+
+        return comments;
+    }
+
+    @GetMapping("/setComment")
+    public String createComment(String commentMessage) {
+        String comment;
+        comment = commentMessage;
+        return "Le commantaire avec le Message : /n" + commentMessage + "/n a bien était ajouté";
+    }
+}
