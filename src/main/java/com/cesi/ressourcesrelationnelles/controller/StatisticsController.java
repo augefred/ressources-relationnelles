beac57d914bc77ce4446616da12b433ddd55f7ac
Flodeco <flodeco22@outlook.fr>
2022-01-12 09:25:26 +0100

    Retourne liste lors de l'appelle de la route /statistics

--- src/main/java/com/cesi/ressourcesrelationnelles/controller/StatisticsController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/controller/StatisticsController.java
@@ -1,12 +1,19 @@
 package com.cesi.ressourcesrelationnelles.controller;
 
+import com.cesi.ressourcesrelationnelles.domain.Statistic;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 public class StatisticsController {
     @GetMapping("/statistics")
-    public String index() {
-        return "Voici les statistiques";
+    public ArrayList<Statistic> getALlStatistic() {
+        ArrayList<Statistic> listStats = new ArrayList<>();
+        listStats.add(new Statistic(1, 1, 200));
+        listStats.add(new Statistic(2, 2, 500));
+        listStats.add(new Statistic(10, 15, 2589));
+        return listStats;
     }
 }
