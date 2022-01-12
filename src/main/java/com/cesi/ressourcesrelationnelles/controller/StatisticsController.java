a3daddf1b83b4cb59d55c89dc25d2b28c2e25f39
Flodeco <flodeco22@outlook.fr>
2022-01-12 16:33:46 +0100

    Ajout createStatistic, requête POST

--- src/main/java/com/cesi/ressourcesrelationnelles/controller/StatisticsController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/controller/StatisticsController.java
@@ -1,19 +1,30 @@
 package com.cesi.ressourcesrelationnelles.controller;
 
 import com.cesi.ressourcesrelationnelles.domain.Statistic;
+import com.cesi.ressourcesrelationnelles.service.StatisticService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StatisticsController {
+    private StatisticService statService;
+
+    public StatisticsController(StatisticService statService){
+        this.statService = statService;
+    }
+
     @GetMapping("/statistics")
-    public ArrayList<Statistic> getALlStatistic() {
-        ArrayList<Statistic> listStats = new ArrayList<>();
-        listStats.add(new Statistic(1, 1, 200));
-        listStats.add(new Statistic(2, 2, 500));
-        listStats.add(new Statistic(10, 15, 2589));
-        return listStats;
+    public List<Statistic> getALlStatistic() {
+        return statService.list();
+    }
+
+    @PostMapping("/statistics")
+    public Statistic createStatistic(@RequestBody Statistic stat){
+        return statService.create(stat);
     }
 }
