b78f42a98764e3ca0a9610f7399660bec19dfb4d
Corentin <corentin.castel0@gmail.com>
2022-01-13 10:48:05 +0100

    test pour le getById du service resource

--- src/test/java/com/cesi/ressourcesrelationnelles/service/ResourceServiceTest.java
+++ src/test/java/com/cesi/ressourcesrelationnelles/service/ResourceServiceTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,5 +21,18 @@ public class ResourceServiceTest {
     public void whenApplicationStarts_thenHibernateCreatesInitialRecords(){
         List<Resource> resources = resourceService.list();
         assertEquals(3, resources.size());
+    }
+
+    @Test
+    public void getById(){
+        Optional<Resource> res = resourceService.getById(1);
+        assertEquals(true, res.isPresent());
+        Resource resource = res.get();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        assertEquals(1, resource.getId());
+        assertEquals("titre", resource.getTitle());
+        String date = dateFormat.format(resource.getDatePublication());
+        assertEquals("2022-01-12 12:55:10", date);
+        assertEquals("C:/Users", resource.getUrl());
     }
 }
