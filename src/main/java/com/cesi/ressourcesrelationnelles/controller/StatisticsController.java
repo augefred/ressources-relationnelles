25c720e3301f645e0c84442d89069ed55517b552
Flodeco <flodeco22@outlook.fr>
2022-01-13 15:07:19 +0100

    Ajout get statistic from ID, avec exception et test

--- src/main/java/com/cesi/ressourcesrelationnelles/controller/StatisticsController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/controller/StatisticsController.java
@@ -2,16 +2,17 @@ package com.cesi.ressourcesrelationnelles.controller;
 
 import com.cesi.ressourcesrelationnelles.domain.Statistic;
 import com.cesi.ressourcesrelationnelles.service.StatisticService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class StatisticsController {
     private StatisticService statService;
 
-    public StatisticsController(StatisticService statService){
+    public StatisticsController(StatisticService statService) {
         this.statService = statService;
     }
 
@@ -21,17 +22,26 @@ public class StatisticsController {
     }
 
     @PostMapping("/statistics")
-    public Statistic createStatistic(@RequestBody Statistic stat){
+    public Statistic createStatistic(@RequestBody Statistic stat) {
         return statService.create(stat);
     }
 
     @DeleteMapping("/statistics/{id}")
-    public void deleteStatistic(@PathVariable long id){
+    public void deleteStatistic(@PathVariable long id) {
         statService.delete(id);
     }
 
     @PutMapping("/statistics")
-    public void updateStatistic(@RequestBody Statistic stat){
+    public void updateStatistic(@RequestBody Statistic stat) {
         statService.update(stat);
+    }
+
+    @GetMapping("/statistics/{id}")
+    public Statistic getStatistic(@PathVariable long id) {
+        try {
+            return statService.getStatistic(id);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
     }
 }
--- src/main/java/com/cesi/ressourcesrelationnelles/service/StatisticService.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/service/StatisticService.java
@@ -1,6 +1,7 @@
 package com.cesi.ressourcesrelationnelles.service;
 
 import com.cesi.ressourcesrelationnelles.domain.Statistic;
+import com.cesi.ressourcesrelationnelles.exception.NotFoundException;
 import com.cesi.ressourcesrelationnelles.repository.StatisticRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,29 +15,38 @@ public class StatisticService {
     @Autowired
     private StatisticRepository statisticRepository;
 
-    public StatisticService (StatisticRepository statisticRepository){
+    public StatisticService(StatisticRepository statisticRepository) {
         this.statisticRepository = statisticRepository;
     }
 
-    public List<Statistic> list(){
+    public List<Statistic> list() {
         return this.statisticRepository.findAll();
     }
 
-    public Statistic create(Statistic statistic){
+    public Statistic create(Statistic statistic) {
         return this.statisticRepository.save(statistic);
     }
 
     public void delete(long id) {
         Optional<Statistic> stat = this.statisticRepository.findById(id);
-        if(stat.isPresent()){
+        if (stat.isPresent()) {
             this.statisticRepository.delete(this.statisticRepository.getById(id));
         }
     }
 
     public void update(Statistic statistic) {
         Optional<Statistic> stat = this.statisticRepository.findById(statistic.getId());
-        if(stat.isPresent()){
+        if (stat.isPresent()) {
             this.statisticRepository.save(statistic);
+        }
+    }
+
+    public Statistic getStatistic(long id) throws NotFoundException {
+        Optional<Statistic> stat = this.statisticRepository.findById(id);
+        if (stat.isPresent()) {
+            return stat.get();
+        } else {
+            throw new NotFoundException("Statistique non trouvée");
         }
     }
 }
