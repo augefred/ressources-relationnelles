b1dd2707596a9c30e361645b7c49bcdc363375d3
augefred <frederic.auge@viacesi.fr>
2022-01-13 15:32:45 +0100

    Ajout exception user not found

--- src/main/java/com/cesi/ressourcesrelationnelles/controller/UsersController.java
+++ src/main/java/com/cesi/ressourcesrelationnelles/controller/UsersController.java
@@ -3,6 +3,8 @@ package com.cesi.ressourcesrelationnelles.controller;
 import com.cesi.ressourcesrelationnelles.domain.User;
 import com.cesi.ressourcesrelationnelles.exception.NotFoundException;
 import com.cesi.ressourcesrelationnelles.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,6 +14,7 @@ import java.util.List;
 @RestController
 public class UsersController {
     private final UserService userService;
+    Logger logger = LoggerFactory.getLogger(UsersController.class);
 
     public UsersController(UserService userService) {
         this.userService = userService;
@@ -27,6 +30,7 @@ public class UsersController {
         try {
             return userService.findById(id);
         } catch (NotFoundException e) {
+            logger.warn("user {} not found", id);
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
     }
