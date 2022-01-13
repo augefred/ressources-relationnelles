4e277a49f723464e63d1c5024c1dac40e524044a
paul <xls-pauul-xls@hotmail.fr>
2022-01-13 11:43:22 +0100

    Test for delete And update of the method UpdateComment

--- src/test/java/com/cesi/ressourcesrelationnelles/service/CommentServiceTest.java
+++ src/test/java/com/cesi/ressourcesrelationnelles/service/CommentServiceTest.java
@@ -2,23 +2,30 @@ package com.cesi.ressourcesrelationnelles.service;
 
 import com.cesi.ressourcesrelationnelles.domain.Comment;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CommentServiceTest {
+@SpringBootTest
+class CommentServiceTest {
 
+    @Autowired
     private CommentService commentService;
 
-    public CommentServiceTest(CommentService commentService) {
-        this.commentService = commentService;
-    }
 
     @Test
     void createCommentTest(){
         commentService.createComment(new Comment());
         List<Comment> comments = commentService.list();
         assertEquals(comments.size(), 1);
+    }
+
+    @Test
+    void deleteCommentTest(){
+        commentService.createComment(new Comment());
+        commentService.deleteComment(1);
     }
 }
